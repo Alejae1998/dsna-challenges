232d8d6d8b0bebda15c0a2c959ccf462cdae1625
Alejae1998 <alejagr1998@gmail.com>
2022-09-20 10:57:58 -0700

    titleCase function done and passing test

--- day-01/index.js
+++ day-01/index.js
@@ -3,4 +3,12 @@ function reverseSentence(str) {
     .split(' ')
     .map((word) => word.split('').reverse().join('')).join(' ');
 }
-module.exports = { reverseSentence };
+
+function titleCase(sentence) {
+  const words = sentence.split(' ');
+  for (let i = 0; i < words.length; i++) {
+    words[i]  = words[i][0].toUpperCase() + words[i].substr(1);
+  }
+  return words.join(' ');
+}
+module.exports = { reverseSentence, titleCase };
