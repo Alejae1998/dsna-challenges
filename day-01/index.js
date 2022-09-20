9ba721b59dd7e1bb21bab77ecef94df9a26d1754
Alejae1998 <alejagr1998@gmail.com>
2022-09-20 12:14:04 -0700

    adding anagrams to index.js

--- day-01/index.js
+++ day-01/index.js
@@ -28,13 +28,28 @@ function oddishOrEvenish(number) {
     return 'Invalid Entry';
   }
 }
-function at(arr, index){
-  if (index >= 0){
-    return (arr[index]);
+function at(arr, index) {
+  if (index >= 0) {
+    return arr[index];
   } else if (index < 0) {
-    return (arr[arr.length + index]);
+    return arr[arr.length + index];
   } else {
     return 'Invalid Entry';
   }
 }
-module.exports = { reverseSentence, titleCase, oddishOrEvenish, at };
+function anagrams(wordOne, wordTwo) {
+  const lengthOne = wordOne.length;
+  const lengthTwo = wordTwo.length;
+  if (lengthOne !== lengthTwo) {
+    return 'Invalid Entry';
+  }
+  const arrayOne = wordOne.split('').sort().join('');
+  const arrayTwo = wordTwo.split('').sort().join('');
+
+  if (arrayOne === arrayTwo) {
+    return true;
+  } else {
+    return false;
+  }
+}
+module.exports = { reverseSentence, titleCase, oddishOrEvenish, at, anagrams };
