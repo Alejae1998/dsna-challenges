bdab5542f9e4e6c6d794532f1bbc440035de0b76
Alejae1998 <alejagr1998@gmail.com>
2022-09-21 07:48:08 -0700

    adding more stack and test

--- day-02/Stack.js
+++ day-02/Stack.js
@@ -9,6 +9,22 @@ class Stack {
   push(item) {
     this.#list.push(item);
   }
+  pop() {
+    if (this.#list.length === 0) {
+      return null;
+    } else {
+      this.#list.pop();
+      return this.#list;
+    }
+  }
+
+  peek() {
+    return this.#list[this.#list.length - 1];
+  }
+
+  get list() {
+    return this.#list;
+  }
 }
 
 module.exports = { Stack };
--- day-02/tests/MyDataStructure.test.js
+++ day-02/tests/MyDataStructure.test.js
@@ -1,4 +1,5 @@
 const { MyDataStructure } = require('../MyDataStructure.js');
+const { Stack } = require('../Stack.js');
 
 describe('MyDataStructure', () => {
   it('should add to the front', () => {
@@ -16,5 +17,36 @@ describe('MyDataStructure', () => {
   it('should return the count', () => {
     const myStructure = new MyDataStructure([4, 6, 8]);
     expect(myStructure.count).toBe(3);
+  });
+});
+
+describe('Stack', () => {
+  it('should add an item to the top of the stack', () => {
+    const stack = new Stack();
+
+    stack.push('fox');
+    stack.push('goose');
+    stack.push('lizard');
+    expect(stack.list).toEqual(['fox', 'goose', 'lizard']);
+    stack.pop();
+    expect(stack.list).toEqual(['fox', 'goose']);
+
+    const peek = stack.peek();
+    expect(peek).toBe('goose');
+
+    stack.pop();
+    expect(stack.list).toEqual(['fox']);
+
+    stack.push('llama');
+    expect(stack.list).toEqual(['fox', 'llama']);
+
+    stack.pop();
+    expect(stack.list).toEqual(['fox']);
+
+    const peek2 = stack.peek();
+    expect(peek2).toEqual('fox');
+
+    stack.pop();
+    expect(stack.list).toEqual([]);
   });
 });
