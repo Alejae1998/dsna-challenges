40455279d9e392ac13ffe893dd2b0e4d379a30e7
Alejae1998 <alejagr1998@gmail.com>
2022-09-21 07:30:28 -0700

    refactoring stack.js

--- day-02/Stack.js
+++ day-02/Stack.js
@@ -1,3 +1,14 @@
-class Stack {}
+class Stack {
+  #list = [];
+
+  constructor(list) {
+    if (list) this.#list = list;
+  }
+
+  //PUSH to add an item to the top of the stack
+  push(item) {
+    this.#list.push(item);
+  }
+}
 
 module.exports = { Stack };
